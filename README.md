cb067f395bff5386780e076bd5626bd49dd1d55d
Aiara Lobo Gomes <aiara.lobogomes@maastro.nl>
2023-10-10 12:16:09 +0200

    Tutorial: pseudonymization of medical images

---
+++ README.md
@@ -0,0 +1,9 @@
+# MIE2003 - Imaging Data Management
+
+Repository for the UM course _Imaging Data Management_. It contains, in 
+particular, the material for the following topics:
+
+- Skills training: Pseudonymization of medical images
+- Lecture: Advanced applications - Federated learning
+- Skills training: Privacy-preserving federated machine learning on imaging 
+  datasets
---
+++ week6_skills_training/tutorial/src/helper.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# Based on:
+# https://pydicom.github.io/pydicom/stable/auto_examples/
+
+import os
+import pydicom
+import numpy as np
+
+def load_scan(files_path: int) -> np.array:
+    """ Load DICOM files
+
+    Parameters
+    ----------
+    files_path
+        Directory path of DICOM scans of a particular subject
+
+    Returns
+    -------
+    img3d
+        3D matrix with all scan slices of a particular subject
+    """
+    # Read all slices
+    slices = []
+    for filename in np.sort(os.listdir(files_path)):
+        filename = os.path.join(files_path, filename)
+        slices.append(pydicom.dcmread(filename))
+
+    # Ensure the slices are in the correct order
+    slices = sorted(slices, key=lambda s: s.SliceLocation)
+
+    # Create 3D array
+    img_shape = list(slices[0].pixel_array.shape)
+    img_shape.append(len(slices))
+    img3d = np.zeros(img_shape)
+
+    # Fill 3D array with the images from the files
+    for i, s in enumerate(slices):
+        img2d = s.pixel_array
+        img3d[:, :, i] = img2d
+
+    return img3d
