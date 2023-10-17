cad274e2530375ff21019498804ee7a038fc4a7a
Aiara Lobo Gomes <aiara.lobogomes@maastro.nl>
2023-10-17 23:09:55 +0200

    Tutorial: vantage6 researcher role

---
+++ week7_skills_training/tutorial/src/config.py
@@ -0,0 +1,11 @@
+# config.py
+
+# Central server
+server_url = "http://localhost"
+server_port = 5000
+server_api = "/api"
+privkey_path = None
+
+# Credentials
+username = "username_here"
+password = "password_here"
